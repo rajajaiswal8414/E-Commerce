51b96e93bbd703efbe81035ecf8f090a493a3920
Jaiswal <2440839@cognizant.com>
2025-11-21 21:39:44 +0530

    Added: Cart and CartItem Mapping

--- src/main/java/com/cognizant/ecom/controller/AuthController.java
+++ src/main/java/com/cognizant/ecom/controller/AuthController.java
@@ -24,10 +24,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -78,7 +75,7 @@ public class AuthController {
                 .toList();
 
 //        UserInfoResponse response = new UserInfoResponse(userDetails.getId(), userDetails.getUsername(), roles, jwtToken);
-        UserInfoResponse response = new UserInfoResponse(userDetails.getId(), userDetails.getUsername(), roles);
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId(), userDetails.getUsername(), roles, jwtCookie.toString());
 
 //        return ResponseEntity.ok(response);
         return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,
@@ -86,7 +83,7 @@ public class AuthController {
                 .body(response);
     }
 
-    @PostMapping("/signup")
+    @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest){
         if(userRepository.existsByUserName(signupRequest.getUsername())){
             return ResponseEntity
@@ -131,5 +128,31 @@ public class AuthController {
         user.setRoles(roles);
         userRepository.save(user);
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<?> getUserDetails(Authentication authentication){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+//        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+//        UserInfoResponse response = new UserInfoResponse(userDetails.getId(), userDetails.getUsername(), roles, jwtToken);
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId(), userDetails.getUsername(), roles, jwtCookie.toString());
+
+//        return ResponseEntity.ok(response);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logoutUser(){
+        ResponseCookie cookie = jwtUtils.getCleanCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,
+                cookie.toString())
+                .body(new MessageResponse("You've been logged out!"));
     }
 }
--- src/main/java/com/cognizant/ecom/security/jwt/JwtUtils.java
+++ src/main/java/com/cognizant/ecom/security/jwt/JwtUtils.java
@@ -58,6 +58,12 @@ public class JwtUtils {
                 .build();
     }
 
+    public ResponseCookie getCleanCookie(){
+        return ResponseCookie.from(jwtCookie, null)
+                .path("/api")
+                .build();
+    }
+
     public String generateTokenFromUsername(String username) {
         return Jwts.builder()
                 .subject(username)
