08bf8cbc09625459c75829165cdbf2aa03f26b7a
Raja Jaiswal <143202200+rajajaiswal8414@users.noreply.github.com>
2025-12-18 15:32:37 +0530

    Delete ecom-frontend directory

--- ecom-frontend/src/utils/truncateText.js
+++
@@ -1,7 +0,0 @@
-const truncateText = (text, charLimit = 90) => {
-  if (text?.length <= charLimit) {
-    return text;
-  }
-  return text.slice(0, charLimit) + "...";
-};
-export default truncateText;
